a0355199189a9014aff9848291ad39f6d3e8dba6
watanabe10220912 <watanabe10220912@gmail.com>
2021-12-31 15:12:11 +0900

    start atomic design

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default Home;
